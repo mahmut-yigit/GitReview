9202c6cd117e652ee3bf12f8a37578db9470078a
Mahmut <mhmtygt.3090@gmail.com>
2020-09-29 19:29:38 +0300

    conflict

--- src/Ders1.java
+++ src/Ders1.java
@@ -6,7 +6,7 @@ public class Ders1 {
 		System.out.println("Saat 17:52");
 		System.out.println("Saat 18:46");
 		System.out.println("Saat 19:17 - Iskender");
-		System.out.println("aat 19:24 - Mahmut");
+		System.out.println("aat 19:28 - Mahmut");
 
 	}
 
